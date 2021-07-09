77d04f06200f1c5d81f0f8074ac9f2a73ca1a523
kentaro0901 <ken0901sm@gmail.com>
2021-07-09 22:12:00 +0900

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn qmeshi.wsgi --log-file -
---
+++ qmeshi_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QmeshiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'qmeshi_app'
