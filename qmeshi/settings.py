6b0c50ee58c190cbfde7bd8e362e0f0bca27e91b
kenrato0901 <ken0901sm@gmail.com>
2021-09-24 19:04:42 +0900

    [fix] staticfile読み込み対策

--- qmeshi/settings.py
+++ qmeshi/settings.py
@@ -147,6 +147,8 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #DEBUG = False
