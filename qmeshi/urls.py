6a08106279c422a5459a0df0c36c0e2677dd9f26
kenrato0901 <ken0901sm@gmail.com>
2021-09-24 15:56:49 +0900

    [fix] migrate errorの暫定対処その2

--- qmeshi/urls.py
+++ qmeshi/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('', include('qmeshi_app.urls')), # サイト
-    # path('admin/', admin.site.urls), # admin
-    # path('api/', include('api.urls')), # API
+    path('', include('qmeshi_app.urls')), # サイト
+    path('admin/', admin.site.urls), # admin
+    path('api/', include('api.urls')), # API
 ]
