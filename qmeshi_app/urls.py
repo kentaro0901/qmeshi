05949d1b80e23931e6c596d93d702d1f1f319d59
kenrato0901 <ken0901sm@gmail.com>
2021-09-24 16:14:09 +0900

    [fix] migratioin error対策

--- qmeshi_app/urls.py
+++ qmeshi_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from . import views
+# from . import views
 
 app_name = 'qmeshi_app'
 urlpatterns = [
-    path('menues', views.MenuList.as_view(), name='menu_list'), # メニュー一覧
-    path('menues/<str:date>', views.MenuList.as_view(), name='menu_list'), # メニュー一覧
-    path('about', views.About.as_view(), name='about'), # メニュー一覧
+#    path('menues', views.MenuList.as_view(), name='menu_list'), # メニュー一覧
+#    path('menues/<str:date>', views.MenuList.as_view(), name='menu_list'), # メニュー一覧
+#    path('about', views.About.as_view(), name='about'), # メニュー一覧
 
-#   path('items', views.ItemList.as_view(), name='item_list'), # アイテム一覧
+#    path('items', views.ItemList.as_view(), name='item_list'), # アイテム一覧
 #    path('items/<int:item_id>/impressions', views.ImpressionList.as_view(), name='impression_list'), # 感想一覧
 #    path('items/<int:item_id>/impressions/new', views.ImpressionAdd.as_view(), name='impression_add'), # 感想登録
 ]
