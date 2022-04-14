f84e1667be2189ea0cd99ae5947630d130746da9
kentaro0901 <ken0901sm@gmail.com>
2022-04-14 11:39:48 +0900

    [update] htmlファイルとviews.pyの整理

--- qmeshi_app/views.py
+++ qmeshi_app/views.py
@@ -1,7 +1,9 @@
 import datetime
+import sys
 
 from django.http import HttpResponseServerError
 from django.shortcuts import redirect
+from django.views import debug
 from django.views.decorators.csrf import requires_csrf_token
 from django.views.generic import TemplateView
 
@@ -18,24 +20,21 @@ class MenuList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        date = datetime.datetime.strptime(self.kwargs.get(
-            'date'), "%Y-%m-%d").date() if self.kwargs.get('date') else datetime.date.today()
+        date = datetime.datetime.strptime(self.kwargs.get('date'), "%Y-%m-%d").date() if self.kwargs.get('date') else datetime.date.today()
         weekdays = ['月', '火', '水', '木', '金', '土', '日']
         context['date_str'] = f'{date.strftime("%m月%d日")}（{weekdays[date.weekday()]}）'
         context['date_current'] = date.strftime('%Y-%m-%d')
-        context['date_prev'] = (
-            date - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-        context['date_next'] = (
-            date + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-        cafeterias = []
-        for cafeteria in Cafeteria.objects.all().order_by('priority'):
-            menues = Menu.objects.filter(
-                start_date__lte=date, end_date__gte=date, cafeteria=cafeteria)
-            l_menues = menues[:(menues.count()+1)//2]
-            r_menues = menues[(menues.count()+1)//2:]
-            cafeterias.append(
-                {'obj': cafeteria, 'l_menues': l_menues, 'r_menues': r_menues})
+        context['date_prev'] = (date - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        context['date_next'] = (date + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        cafeterias = Cafeteria.objects.all().order_by('priority')
+        cafeterias_with_menues = []
+        for cafeteria in cafeterias:
+            menues = Menu.objects.filter(start_date__lte=date, end_date__gte=date, cafeteria=cafeteria)
+            menues_left = menues[:(menues.count()+1)//2]
+            menues_right = menues[(menues.count()+1)//2:]
+            cafeterias_with_menues.append({'cafeteria': cafeteria, 'menues_left': menues_left, 'menues_right': menues_right})
         context['cafeterias'] = cafeterias
+        context['cafeterias_with_menues'] = cafeterias_with_menues
         return context
 
 
@@ -49,11 +48,9 @@ class About(TemplateView):
         return context
 
 
-# 本番環境でサーバーエラーの詳細を表示する
+# 本番環境でサーバーエラーの詳細を表示する関数
+# 表示する場合はqmeshi/url.pyのコメントアウトをはずす
 @requires_csrf_token
 def my_customized_server_error(request, template_name='500.html'):
-    import sys
-
-    from django.views import debug
     error_html = debug.technical_500_response(request, *sys.exc_info()).content
     return HttpResponseServerError(error_html)
